c30e780acb31b231425639adde9f537b014cef71
coral <jonas@jonasbengtson.se>
2015-11-25 11:48:25 +0100

    ok wat

--- index.js
+++ index.js
@@ -15,3 +15,4 @@ var server = require('./lib/server')
     });
 
 server.start();
+
--- static/js/sip.js
+++ static/js/sip.js
@@ -1,56 +1,54 @@
-// Create our JsSIP instance and run it:
+$( document ).ready(function() {
+  var config = {
+    // Replace this IP address with your Asterisk IP address
+    uri: '1060@77.80.228.235',
 
-var configuration = {
-  'ws_servers': 'ws://10.0.1.43:8088/ws',
-  'uri': 'sip:alice@example.com',
-  'password': 'superpassword'
-};
+    // Replace this IP address with your Asterisk IP address,
+    // and replace the port with your Asterisk port from the http.conf file
+    ws_servers: 'wss://77.80.228.235:9000/ws',
 
-var ua = new JsSIP.UA(configuration);
+    // Replace this with the username from your sip.conf file
+    authorizationUser: '1060',
 
-ua.start();
+    // Replace this with the password from your sip.conf file
+    password: 'password',
 
+    // HackIpInContact for Asterisk
+    hackIpInContact: true,
 
-// Make an audio/video call:
-var session = null;
+  };
 
-// HTML5 <video> elements in which local and remote video will be shown
-var selfView =   document.getElementById('my-video');
-var remoteView =  document.getElementById('peer-video');
+  var ua = new SIP.UA(config);
 
-// Register callbacks to desired call events
-var eventHandlers = {
-  'progress': function(e){
-    console.log('call is in progress');
-  },
-  'failed': function(e){
-    console.log('call failed with cause: '+ e.data.cause);
-  },
-  'ended': function(e){
-    console.log('call ended with cause: '+ e.data.cause);
-  },
-  'confirmed': function(e){
-    var local_stream = session.connection.getLocalStreams()[0];
+  // Invite with audio only
+  var options = {
+          media: {
+              constraints: {
+                  audio: true,
+                  video: false
+              },
+              render: {
+                  remote: document.getElementById('remoteAudio'),
+                  local: document.getElementById('localAudio')
+              }
+          }
+      };
 
-    console.log('call confirmed');
-
-    // Attach local stream to selfView
-    selfView = JsSIP.rtcninja.attachMediaStream(selfView, local_stream);
-  },
-  'addstream': function(e){
-    var stream = e.stream;
-
-    console.log('remote stream added');
-
-    // Attach remote stream to remoteView
-    remoteView = JsSIP.rtcninja.attachMediaStream(remoteView, stream);
-  }
-};
-
-var options = {
-  'eventHandlers': eventHandlers,
-  'mediaConstraints': {'audio': true, 'video': true}
-};
+  session = ua.invite('1062', options, function(lol) {
+    console.log("HEHEHEHE");
+  });
 
 
-session = ua.call('sip:bob@example.com', options);
+
+
+  ua.on('invite', function (session) {
+      session.accept({
+          media: {
+              render: {
+                  remote: document.getElementById('remoteVideo'),
+                  local: document.getElementById('localVideo')
+              }
+          }
+      });
+  });
+});
